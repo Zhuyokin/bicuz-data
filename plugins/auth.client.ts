6ed7c21dd8b38df2ce300d53b8d77825521c943d
zhuyujin <yokinzhu@gmail.com>
2024-08-20 16:26:53 +0800

    fix: 'token问题'

--- plugins/auth.client.ts
+++ plugins/auth.client.ts
@@ -1,13 +1,18 @@
-import { useUserStore } from '@/store';
+import { useUserStore } from '@/store'
 
 export default defineNuxtPlugin(() => {
   const route = useRoute()
   const userStore = useUserStore()
-  let token = route.query.token as string;
+  let token = route.query.token as string
   console.log('[ url中的token ] >', token)
-  if (token) {
+  if (Array.isArray(token) && token) {
     // token中的+号会丢失，这里将token中的空格转换成+号
-    token = token.replace(/\s/g, '+');
+    token[0] = token[0].replace(/\s/g, '+')
+    userStore.setToken(token[0])
+  }
+  else if (token) {
+    // token中的+号会丢失，这里将token中的空格转换成+号
+    token = token.replace(/\s/g, '+')
     userStore.setToken(token)
   }
 })
