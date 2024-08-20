69ab5016ac472d5e64cc076258e1d090e20192ab
yyb <531728859@qq.com>
2024-08-20 19:32:43 +0800

    fix: 修复兼容问题

--- plugins/core-js-polyfill.ts
+++ plugins/core-js-polyfill.ts
@@ -1,10 +1,9 @@
-import { cloneDeep } from 'lodash'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(() => {
   if (typeof globalThis.structuredClone === 'undefined') {
     globalThis.structuredClone = (obj) => {
-      return cloneDeep(obj)
+      return useCloneDeep(obj)
     }
   }
 })
